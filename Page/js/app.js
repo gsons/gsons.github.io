9fb4e8bd0b159851aa38f582d58510a174f970ba
gsons <976955017@qq.com>
2017-01-24 16:25:07 +0800

    now
    now

--- Page/js/app.js
+++ Page/js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	var p1=initpage1();
 	var p2=initpage2();
@@ -8,8 +9,9 @@ $(document).ready(function(){
 	p1.show();
 	//适配手机
 	 var fontsize=screen.width*100/412;
-	//var fontsize2=screen.height*100/732;
 	$("html").css("font-size",fontsize+"px");
+
+
 	$("#audio").click(function(){
 	$(this).addClass("musicplay");
 	if(audio.paused){
@@ -21,24 +23,48 @@ $(document).ready(function(){
 		$(this).removeClass("musicplay");
 	}
 });
+//当前页码
+var currentpage=1;
 
-	p1.click(function(){
-		p1.hide();
-		p2.show();
-		p2.addClass("rotate-right");
-	});
+$("#prev").hide();
+$("#next").click(function(){
+	$("#prev").show();
+	if(currentpage<4) {
+		$("#page"+currentpage).hide();
+		currentpage++;
+		if(currentpage==4) $("#next").hide();
+		$("#page"+currentpage).show();
 
-	p2.click(function(){
-	p2.hide();
-	p3.show();
-	p3.addClass("flippage-normalTranslateInUp");
-   });
+		if(currentpage==2)
+		$("#page"+currentpage).addClass("flippage-normalTranslateInUp");
+	    else if(currentpage==3)
+		$("#page"+currentpage).addClass("rotate-right");
+	    else if(currentpage==4)
+		$("#page"+currentpage).addClass("flippage-cutCard-top-upward ");
+	}
 
-	p3.click(function(){
-	p3.hide();
-	p4.show();
-	p4.addClass("flippage-cutCard-top-upward ");
-   });
+});
+
+$("#prev").click(function(){
+	$("#next").show();
+	if(currentpage>1) {
+		$("#page"+currentpage).hide();
+		currentpage--;
+		if(currentpage==1) $("#prev").hide();
+		$("#page"+currentpage).show();
+
+
+		if(currentpage==2)
+		$("#page"+currentpage).addClass("flippage-normalTranslateInUp");
+	    else if(currentpage==3)
+		$("#page"+currentpage).addClass("rotate-right");
+	    else if(currentpage==4)
+		$("#page"+currentpage).addClass("flippage-cutCard-top-upward ");
+	}
+});
+	
+});
+
 	/**
 	 * 初始化page1
 	 * @return {[void]} [description]
@@ -48,8 +74,8 @@ $(document).ready(function(){
 	var text_2=new Item("text_ji","./image/text_ji.png",87,123,180,270,"zoomInUp 1.4s",0);
 	var text_3=new Item("text_he","./image/text_he.png",148,211,80,320,"zoomInUp 1.6s",0);
 	var text_4=new Item("text_sui","./image/text_sui.png",235,272,100,380,"scaleInCenter .9s",0);
-	var cloud1=new Item("cloud1","./image/cloud1.png",162,80,0,280,"shake 7.5s linear infinite .3s",0);
-	var cloud2=new Item("cloud2","./image/cloud2.png",162,80,300,550,"shake 7.5s linear infinite .3s",0);
+	var cloud1=new Item("cloud1","./image/cloud1.png",162,80,0,280,"shake 8.5s linear infinite .3s",0);
+	var cloud2=new Item("cloud2","./image/cloud2.png",162,80,300,550,"shake 8.5s linear infinite .3s",0);
       var bg= new BgItem("bg","./image/p1_bg.jpg","120%","",0);
        var snow= new BgItem("snow","./image/snow.gif","100%","",1);
         var p1=new Page("page1");
@@ -60,10 +86,9 @@ $(document).ready(function(){
 			lantern[i]=new Item("lantern"+i,"./image/lantern.png",65,62,i*80,10,"swingLR 5s linear infinite",0);
 			p1.addElement(lantern[i]);
 		}
-	 p1.addElement(text_1);
-	 p1.addElement(text_2);
-	 p1.addElement(text_3);
-	 p1.addElement(text_4);
+	 var items=[text_1,text_2,text_3,text_4];//定义item数组
+	  var times=[0,300,300,300];//定义时间间隔数组
+	 sequence_active(p1,items,times);
 	 p1.addElement(cloud1);
 	 p1.addElement(cloud2);
 	 return $("#"+p1.id);
@@ -73,61 +98,52 @@ $(document).ready(function(){
 	 * @return {[void]} [description]
 	 */
 	  function initpage2(){
-	var text_1=new Item("text_jin","./image/text_jin.png",176,189,100,120,"zoomInDown 1.2s",0);
-	var text_2=new Item("text_ji","./image/text_ji.png",87,123,180,270,"zoomInUp 1.4s",0);
-	var text_3=new Item("text_he","./image/text_he.png",148,211,80,320,"zoomInUp 1.6s",0);
-	var text_4=new Item("text_sui","./image/text_sui.png",235,272,100,380,"scaleInCenter .5s",0);
-	var text_4=new Item("text_sui","./image/text_sui.png",235,272,100,380,"scaleInCenter .5s",0);
-	var couplet1=new Item("couplet1","./image/couplet.png",130,500,0,120,"scaleInCenter .5s",0);
-	var couplet2=new Item("couplet2","./image/couplet.png",130,500,280,120,"scaleInCenter .5s",0);
-	var couplet_1=new Item("couplet_1","./image/couplet_1.png",57,450,40,140,"scaleInCenter .5s linear 1s",0);
-	var couplet_2=new Item("couplet_2","./image/couplet_2.png",57,450,320,140,"scaleInCenter .5s linear 1s",0);
-     var bg= new BgItem("bg","./image/p2_bg.jpg","120%","",0)
-        var snow= new BgItem("snow","./image/snow.gif","120%","",1);
-        var p2=new Page("page2");
+	var lantern=new Item("lantern","./image/p1_lantern.png",150,390,140,0,"rubberJelly .5s",0);
+    var bg= new BgItem("bg","./image/p3_bg.jpg","120%","",0)
+    var snow= new BgItem("snow","./image/snow.gif","120%","",1);
+    var imooc=new Item("blessing","./image/p2_imooc.png",89,106,175,500," rubberJelly 2s linear 1s",0);
+    var p2=new Page("page2");
      p2.addBgItem(bg);
      p2.addBgItem(snow);
-	 p2.addElement(text_1);
-	 p2.addElement(text_2);
-	 p2.addElement(text_3);
-	 p2.addElement(text_4);
-	 p2.addElement(couplet1);
-	 p2.addElement(couplet2);
-	  p2.addElement(couplet_1);
-	  p2.addElement(couplet_2);
+      p2.addElement(imooc);
+     p2.addElement(lantern);
+       p2.addElement(lantern);
+        p2.addElement(lantern);
 	 return $("#"+p2.id);
 	 }
 
+	 /**
+	  * 初始化page3
+	  * @return {[type]} [description]
+	  */
 	function initpage3(){
-	var text_1=new Item("text_jin","./image/text_jin.png",176,189,100,120-80,"zoomInDown 1.2s",0);
-	var text_2=new Item("text_ji","./image/text_ji.png",87,123,180,270-80,"zoomInUp 1.4s",0);
-	var text_3=new Item("text_he","./image/text_he.png",148,211,80,320-80,"zoomInUp 1.6s",0);
-	var text_4=new Item("text_sui","./image/text_sui.png",235,272,100,380-80,"scaleInCenter .5s",0);
-	var couplet1=new Item("couplet1","./image/couplet.png",130,500,0,120-80,"scaleInCenter .5s",0);
-	var couplet2=new Item("couplet2","./image/couplet.png",130,500,280,120-80,"scaleInCenter .5s",0);
-	var couplet_1=new Item("couplet_1","./image/couplet_1.png",57,480,40,120-80,"scaleInCenter .5s linear",0);
-	var couplet_2=new Item("couplet_2","./image/couplet_2.png",57,480,320,120-80,"scaleInCenter .5s linear",0);
-    var blessing=new Item("blessing","./image/p3_blessing.png",120,120,135,510,"rotate 2.5s linear infinite",2);
+	var text_1=new Item("text_jin","./image/text_jin.png",176,189,100,100,"zoomInDown 1.4s",0);
+	var text_2=new Item("text_ji","./image/text_ji.png",87,123,180,250,"zoomInUp 1.4s",0);
+	var text_3=new Item("text_he","./image/text_he.png",148,211,80,300,"zoomInUp 1.4s",0);
+	var text_4=new Item("text_sui","./image/text_sui.png",235,272,100,360,"scaleInCenter 1.4s",0);
+	var couplet1=new Item("couplet1","./image/couplet.png",130,500,0,100,"scaleInCenter .5s",0);
+	var couplet2=new Item("couplet2","./image/couplet.png",130,500,280,100,"scaleInCenter .5s",0);
+	var couplet_1=new Item("couplet_1","./image/couplet_1.png",57,460,40,115,"scaleInCenter .5s linear",0);
+	var couplet_2=new Item("couplet_2","./image/couplet_2.png",57,460,320,115,"scaleInCenter .5s linear",0);
      var bg= new BgItem("bg","./image/p3_bg.jpg","120%","",0)
     var snow= new BgItem("snow","./image/snow.gif","120%","",1);
     var p3=new Page("page3");
      p3.addBgItem(bg);
      p3.addBgItem(snow);
-	 p3.addElement(text_1);
-	 p3.addElement(text_2);
-	 p3.addElement(text_3);
-	 p3.addElement(text_4);
-	 p3.addElement(couplet1);
-	 p3.addElement(couplet2);
-	  p3.addElement(couplet_1);
-	   p3.addElement(blessing);
-	   p3.addElement(couplet_2);
+	  var items=[text_1,text_2,text_3,text_4,couplet1,couplet_1,couplet2,couplet_2];//定义item数组
+	  var times=[5000,200,200,200,600,600,600,600];//定义时间间隔数组
+	 sequence_active(p3,items,times);
 	 return $("#"+p3.id);
 	 }
+
+	 /**
+	  * 初始化page3
+	  * @return {[type]} [description]
+	  */
 	function initpage4(){
 	var text_1=new Item("p4_happy","./image/happy.png",283,58,70,120,"zoomInDown 1.2s",0);
 	var text_2=new Item("spring","./image/spring.png",188,36,100,200,"zoomInUp 1.4s",0);
-	//
+	var blessing=new Item("blessing","./image/p3_blessing.png",130,130,150,250,"rotate 1.4s linear infinite",0);
 	var people=new Item("people","./image/people.png",400,390,0,screen.height-screen.width*400/412,"zoomInUp 1.4s",0);
       var bg= new BgItem("bg","./image/p1_bg.jpg","120%","",0);
        var snow= new BgItem("snow","./image/snow.gif","100%","",1);
@@ -142,11 +158,11 @@ $(document).ready(function(){
 	 p4.addElement(text_1);
 	 p4.addElement(text_2);
 	  p4.addElement(people);
+	   p4.addElement(blessing);
+	  $(".people").css({"top":"","bottom":"-0.2rem"});
 	 return $("#"+p4.id);
-	 }
-	 function addFixedItem(){
-	 	
-	 }
+	 }	
+
 	/**
 	 * 页面构造函数
 	 * @param {[string]} id    [页面id]
@@ -186,15 +202,31 @@ $(document).ready(function(){
 		$("#"+this.id+" ."+item.mclass).css("background","url('"+item.imgsrc+"') no-repeat center center");
 		$("#"+this.id+" ."+item.mclass).css("background-size","100%");
 		if(item.animation!="")$("#"+this.id+" ."+item.mclass).css("animation",item.animation);
+	 }
+	 /**
+	 * 串行动作
+	 * @param  {[type]} page  [description]
+	 * @param  {[type]} items [description]
+	 * @param  {Number} i     [description]
+	 * @return {[type]}       [description]
+	 */
+	/*this.sequence_active=function (items,i=0){
+		if(i==items.length) {i=0;return;}
+		setTimeout(function(){
+			this.addElement(items[i]);this.sequence_active(items,i+1);
+		},200);
+	}*/
+	//显示页面
+   this.show=function(){
+   	 $("#"+this.id).show();
+   }
+   //隐藏页面
+   this.hide=function(){
+   	 $("#"+this.id).hide();
+   }
+    this.hide();
 	}
-	   this.show=function(){
-	   	 $("#"+this.id).show();
-	   }
-	   this.hide=function(){
-	   	 $("#"+this.id).hide();
-	   }
-	  this.hide();
-	}
+
 	/**
 	 * 背景精灵构造函数
 	 * @param {[string]} mclass    [类名]
@@ -231,4 +263,16 @@ $(document).ready(function(){
 		this.animation=animation;
 		this.z_index=z_index;
 	}
-});
+	/**
+	 * 串行动作
+	 * @param  {[page]} page  [页面对象]
+	 * @param  {[array]} items [item数组]
+	 * @param  {Number} i     [时间间隔]
+	 * @return {[array]}       [遍历数组]
+	 */
+	function sequence_active(page,items,times,i=0){
+	if(i==items.length) {i=0;return;}
+	setTimeout(function(){
+		page.addElement(items[i]);sequence_active(page,items,times,i+1);
+	},times[i]);
+   }
