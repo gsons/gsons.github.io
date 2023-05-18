560f5a5c7603f0409e0e66fbbe8898103191338e
Gsonhub <976955017@qq.com>
2023-05-18 21:39:04 +0800

    Update domain.js

--- domain.js
+++ domain.js
@@ -1,1 +1,1 @@
-get_domain('{"domain":["65ca214d.r3.cpolar.cn","5776e864.r2.cpolar.cn","t6rz7no.nat.ipyingshe.com"]}');
+get_domain('{"domain":["5776e864.r2.cpolar.cn","65ca214d.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]}');
