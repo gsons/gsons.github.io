d72b746fb16d1cd49aa55e58300794f5942367fa
Gsonhub <976955017@qq.com>
2023-05-22 21:27:35 +0800

    Update get_domain_list.js

--- get_domain_list.js
+++ get_domain_list.js
@@ -1,1 +1,1 @@
-get_domain_list(["7e1e5fc0.r2.cpolar.cn","5e90acae.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]);
+get_domain_list(["7e1e5fc0.r2.cpolar.cn","7e685e55.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]);
