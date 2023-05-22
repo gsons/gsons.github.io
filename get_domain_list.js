5c61f5668576f2880419185e287783efca513daa
Gsonhub <976955017@qq.com>
2023-05-22 18:04:20 +0800

    Update get_domain_list.js

--- get_domain_list.js
+++ get_domain_list.js
@@ -1,1 +1,1 @@
-get_domain_list(["22def942.r3.cpolar.cn","50e3f298.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]);
+get_domain_list(["22def942.r3.cpolar.cn","5e90acae.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]);
