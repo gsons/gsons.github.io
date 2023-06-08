4bb19106de99996d4932c1208700f3111f2d24da
Gsonhub <976955017@qq.com>
2023-06-08 16:49:14 +0800

    Update get_domain_list.js

--- get_domain_list.js
+++ get_domain_list.js
@@ -1,1 +1,1 @@
-get_domain_list(["78c500a8.r3.cpolar.cn","54c436dc.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]);
+get_domain_list(["49b7e988.r2.cpolar.cn","54c436dc.r3.cpolar.cn","t6rz7no.nat.ipyingshe.com"]);
